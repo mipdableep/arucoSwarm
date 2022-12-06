911c65fa23e4eaa7946b470d78acd15e53f7f913
mipdableep <idodo.talmor@gmail.com>
2022-12-06 20:39:21 +0200

    started server instructions, project automation

--- bashrc_extension.sh
+++ bashrc_extension.sh
@@ -1,4 +1,7 @@
 export LD_LIBRARY_PATH=/usr/local/lib
 while ! [[ $(ip a | grep "inet 192") ]]; do sleep 1s; done
+
+ipServerHost=192.168.138.125
+
 cd /home/pi/arucoSwarm/build
-/home/pi/arucoSwarm/build/followAruco /home/pi/arucoSwarm/mobilenet_ssd_v2_coco_quant_postprocess.tflite 192.168.138.125 9090 2>&1 | tee out.txt
+/home/pi/arucoSwarm/build/followAruco /home/pi/arucoSwarm/mobilenet_ssd_v2_coco_quant_postprocess.tflite $ipServerHost 9090 2>&1 | tee out.txt
