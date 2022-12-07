53fa9c6c669761bba5f6c665e7224bba4ccfaaae
mipdableep <idodo.talmor@gmail.com>
2022-12-07 13:52:43 +0200

    script changes

--- scripts/change_tello_conf.sh
+++ scripts/change_tello_conf.sh
@@ -2,5 +2,7 @@ cd change_tello_conf/
 python3 maker.py
 sudo rm /etc/wpa_supplicant/tello.conf 
 sudo mv tello.conf /etc/wpa_supplicant/
-echo "/etc/wpa_supplicant/tello.conf:"
+
+echo -e "\n/etc/wpa_supplicant/tello.conf:\n"
 cat /etc/wpa_supplicant/tello.conf
+echo -e '\n'
