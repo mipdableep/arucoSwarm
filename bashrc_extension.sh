64b4a5e8bb9c9f700db8379f1ee36958ace40198
mipdableep <idodo.talmor@gmail.com>
2022-12-13 12:59:40 +0200

    added chroot identifier in bashrc_extention

--- bashrc_extension.sh
+++ bashrc_extension.sh
@@ -1,8 +1,20 @@
 cd /home/pi/arucoSwarm
-export LD_LIBRARY_PATH=/usr/local/lib
-while ! [[ $(ip a | grep "inet 192") ]]; do sleep 1s; done
 
-ipServerHost=192.168.43.163
+ls /dev/ > scripts/compare.txt
 
-cd /home/pi/arucoSwarm/build
-/home/pi/arucoSwarm/build/followAruco /home/pi/arucoSwarm/mobilenet_ssd_v2_coco_quant_postprocess.tflite $ipServerHost 9090 2>&1 | tee out.txt
+diff -q scripts/devls.txt scripts/compare.txt
+if [[ $? == "0" ]]
+then
+    echo -e "\nin chroot, breaking\n"
+    cd /home/pi
+else
+
+    export LD_LIBRARY_PATH=/usr/local/lib
+    while ! [[ $(ip a | grep "inet 192") ]]; do sleep 1s; done
+
+    ipServerHost=192.168.43.163
+
+    cd /home/pi/arucoSwarm/build
+    /home/pi/arucoSwarm/build/followAruco /home/pi/arucoSwarm/mobilenet_ssd_v2_coco_quant_postprocess.tflite $ipServerHost 9090 2>&1 | tee out.txt
+    
+fi
