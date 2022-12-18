ea1b1bca0e62c2b0799b3ba880c6326eb1a62e9c
mipdableep <idodo.talmor@gmail.com>
2022-12-19 00:31:14 +0200

    red and green in led, need to solder, change pins

---
+++ scripts/led/green.py
@@ -0,0 +1,23 @@
+# External module imports
+import RPi.GPIO as GPIO
+import time
+
+# Pin Definitons:
+ledPin = ***
+
+# Pin Setup:
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(ledPin, GPIO.OUT) # LED pin set as output
+
+cleanUp = GPIO.cleanup()
+
+counter = 0
+try:
+    GPIO.output(ledPin, GPIO.HIGH)
+    time.sleep(15)
+except (KeyboardInterrupt):
+    GPIO.cleanup()
+    exit(0)
+
+GPIO.cleanup()
+
