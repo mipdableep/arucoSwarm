c61e065e10c0476fbe6a600ffbd0f0c02d34d14c
mipdableep <idodo.talmor@gmail.com>
2022-12-19 12:26:09 +0200

    cleanup clears board setup

--- hardtest/ledRelease.py
+++ hardtest/ledRelease.py
@@ -9,8 +9,6 @@ ledPin = 12 # Broadcom pin 23 (P1 pin 16)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(ledPin, GPIO.OUT) # LED pin set as output
 
-cleanUp = GPIO.cleanup()
-
 counter = 0
 try:
     while counter < 20:
